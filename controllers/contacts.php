2927964085a527112885462ac274d984911955f8
Rasmus Fuhse <Krassmus@gmail.com>
2013-03-05 09:48:14 +0100

    crack magic-public-key

--- controllers/contacts.php
+++ controllers/contacts.php
@@ -27,8 +27,11 @@ class ContactsController extends ApplicationController {
         $rsa = new Crypt_RSA();
         $key = "RSA.8zK369nRrd2grj5BO3izZt9AsHZvOu4oouLPed-jgjC1LfTMg210jK3vf7t3ZjdAhRmF7sgnhvas-4SNSta-8S84w4xDuHpqutNEBNhirFFEBbGD-y0l1eyvPaFwG9-7H5nVT9FeV9dcaBUo6v4bV7kkj_3x5J85yZROjYVKdas=.AQAB";
         $key = explode(".", $key);
-        $exponent = MagicSignature::base64_url_decode($key[2]);
-        $rsa->loadKey(MagicSignature::base64_url_decode($key[1]), CRYPT_RSA_PUBLIC_FORMAT_RAW);
+        $raw_key = array(
+            'modulus' => new Math_BigInteger(MagicSignature::base64_url_decode($key[1]), 256),
+            'exponent' => new Math_BigInteger($key[2] === "AQAB" ? 65537 : MagicSignature::base64_url_decode($key[2]))
+        );
+        $rsa->loadKey($raw_key, CRYPT_RSA_PUBLIC_FORMAT_RAW);
         var_dump($rsa);
         
         $this->contacts = OstatusContact::findMine();
---
+++ views/salmon/envelope.php
@@ -0,0 +1,7 @@
+<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<me:env xmlns:me="http://salmon-protocol.org/ns/magic-env">
+    <me:data type="application/atom+xml"><?= htmlReady($base64data) ?></me:data>
+    <me:encoding>base64url</me:encoding>
+    <me:alg>RSA-SHA256</me:alg>
+    <me:sig><?= htmlReady($sig) ?></me:sig>
+</me:env>
