85c527b40f82f0c892df817226350594d62a7af4
Rasmus Fuhse <Krassmus@gmail.com>
2013-03-15 20:51:07 +0100

    html in feed

--- views/webfinger/feed.php
+++ views/webfinger/feed.php
@@ -6,14 +6,14 @@
             xmlns:poco="http://portablecontacts.net/spec/1.0" 
             xmlns:ostatus="http://ostatus.org/schema/1.0" 
             xmlns:statusnet="http://status.net/schema/api/1/">
-  <generator uri="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>" version="<?= $GLOBALS['SOFTWARE_VERSION'] ?>"><?= utf8_encode(htmlReady($GLOBALS['UNI_NAME_CLEAN'])) ?></generator>
+  <generator uri="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>" version="<?= $GLOBALS['SOFTWARE_VERSION'] ?>"><?= studip_utf8encode(htmlReady($GLOBALS['UNI_NAME_CLEAN'])) ?></generator>
   <id><?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/webfinger/feed/".$user['username']) ?></id>
-  <title><?= utf8_encode(htmlReady($user->getName())) ?> timeline</title>
-  <subtitle>Updates from <?= utf8_encode(htmlReady($user->getName())) ?> on <?= utf8_encode(htmlReady($GLOBALS['UNI_NAME_CLEAN'])) ?>!</subtitle>
+  <title><?= studip_utf8encode(htmlReady($user->getName())) ?> timeline</title>
+  <subtitle>Updates from <?= studip_utf8encode(htmlReady($user->getName())) ?> on <?= studip_utf8encode(htmlReady($GLOBALS['UNI_NAME_CLEAN'])) ?>!</subtitle>
   <logo><?= $user->getAvatar()->getURL(AVATAR::NORMAL) ?></logo>
   <updated>2010-08-22T13:26:16+00:00</updated>
   <author>
-    <name><?= utf8_encode(htmlReady($user->getName())) ?></name>
+    <name><?= studip_utf8encode(htmlReady($user->getName())) ?></name>
     <uri><?= $GLOBALS['ABSOLUTE_URI_STUDIP'].'dispatch.php/profile?username='.$user['username'] ?></uri>
   </author>
   <link href="<?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/profile?user_id=".$user['user_id']) ?>" rel="alternate" type="text/html"/>
@@ -24,13 +24,13 @@
   <link href="<?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/webfinger/feed/".$user['username']) ?>" rel="self" type="application/atom+xml"/>
   <? foreach ($blubber as $blubb) : ?>
   <entry>
-    <title><?= utf8_encode(htmlReady($blubb['name'])) ?></title>
+    <title><?= studip_utf8encode(htmlReady($blubb['name'])) ?></title>
     <link rel="alternate" type="text/html" href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/thread/".$blubb['root_id'] ?>"/>
     <id><?= $blubb['root_id'] === $blubb['topic_id'] ? $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/thread/".$blubb['root_id'] : $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/comment/".$blubb->getId() ?></id>
     <published><?= date("c", $blubb['mkdate']) ?></published>
     <updated><?= date("c", $blubb['chdate']) ?></updated>
     <content type="markdown"><?= studip_utf8encode(htmlReady($blubb['description'])) ?></content>
-    <content type="html"><?= studip_utf8encode(formatReady($blubb['description'])) ?></content>
+    <content type="html"><?= studip_utf8encode(htmlReady(formatReady($blubb['description']))) ?></content>
     <activity:verb>http://activitystrea.ms/schema/1.0/post</activity:verb>
     <activity:object-type><?= $blubb['root_id'] === $blubb['topic_id'] ? "http://activitystrea.ms/schema/1.0/note" : "http://activitystrea.ms/schema/1.0/comment" ?></activity:object-type>
     <statusnet:notice_info local_id="<?= $blubb->getId() ?>" source="web"></statusnet:notice_info>
