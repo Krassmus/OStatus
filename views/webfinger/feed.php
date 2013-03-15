f3253f056e70d1ee4c8206ee9a79ca763bc906bc
Rasmus Fuhse <Krassmus@gmail.com>
2013-03-15 20:44:07 +0100

    update user-feed

--- views/webfinger/feed.php
+++ views/webfinger/feed.php
@@ -16,9 +16,8 @@
     <name><?= utf8_encode(htmlReady($user->getName())) ?></name>
     <uri><?= $GLOBALS['ABSOLUTE_URI_STUDIP'].'dispatch.php/profile?username='.$user['username'] ?></uri>
   </author>
-  <link href="http://identi.ca/testkoski" rel="alternate" type="text/html"/>
-  <link href="http://identi.ca/main/sup#209172" rel="http://api.friendfeed.com/2008/03#sup" type="application/json"/>
-  <link href="http://identi.ca/main/push/hub" rel="hub"/>
+  <link href="<?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/profile?user_id=".$user['user_id']) ?>" rel="alternate" type="text/html"/>
+  <link href="<?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/hub/register") ?>" rel="hub"/>
   <link href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>plugins.php/ostatus/salmon/endpoint" rel="salmon"/>
   <link href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>plugins.php/ostatus/salmon/endpoint" rel="http://salmon-protocol.org/ns/salmon-replies"/>
   <link href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>plugins.php/ostatus/salmon/endpoint" rel="http://salmon-protocol.org/ns/salmon-mention"/>
@@ -27,10 +26,11 @@
   <entry>
     <title><?= utf8_encode(htmlReady($blubb['name'])) ?></title>
     <link rel="alternate" type="text/html" href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/thread/".$blubb['root_id'] ?>"/>
-    <id><?= $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/thread/".$blubb['root_id'] ?></id>
+    <id><?= $blubb['root_id'] === $blubb['topic_id'] ? $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/thread/".$blubb['root_id'] : $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/comment/".$blubb->getId() ?></id>
     <published><?= date("c", $blubb['mkdate']) ?></published>
     <updated><?= date("c", $blubb['chdate']) ?></updated>
-    <content type="html"><?= utf8_encode(htmlReady($blubb['description'])) ?></content>
+    <content type="markdown"><?= studip_utf8encode(htmlReady($blubb['description'])) ?></content>
+    <content type="html"><?= studip_utf8encode(formatReady($blubb['description'])) ?></content>
     <activity:verb>http://activitystrea.ms/schema/1.0/post</activity:verb>
     <activity:object-type><?= $blubb['root_id'] === $blubb['topic_id'] ? "http://activitystrea.ms/schema/1.0/note" : "http://activitystrea.ms/schema/1.0/comment" ?></activity:object-type>
     <statusnet:notice_info local_id="<?= $blubb->getId() ?>" source="web"></statusnet:notice_info>
