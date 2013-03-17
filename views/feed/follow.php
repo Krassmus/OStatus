3dbf8a292a5c64efefd5a3e5ea30aae71a45b856
Rasmus Fuhse <Krassmus@gmail.com>
2013-03-17 15:08:03 +0100

    htmlspecialchars instead of htmlReady in XML-templates

--- views/feed/follow.php
+++ views/feed/follow.php
@@ -1,11 +1,11 @@
 <?= '<?xml version="1.0" encoding="UTF-8"?>'."\n" ?>
 <entry xmlns="http://www.w3.org/2005/Atom" xmlns:activity="http://activitystrea.ms/spec/1.0/">
   <id><?= $GLOBALS['ABSOLUTE_URI_STUDIP']."dispatch.php/profile?username=".$user['username'] ?></id>
-  <title><?= htmlReady($user->getName()) ?> is now following <?= htmlReady($whiterabbit->getName()) ?></title>
+  <title><?= htmlspecialchars($user->getName()) ?> is now following <?= htmlspecialchars($whiterabbit->getName()) ?></title>
   <author>
-    <uri>acct:<?= htmlReady($user['username']) ?>@<?= $_SERVER['SERVER_NAME'] ?></uri>
-    <name><?= htmlReady($user->getName())?></name>
-    <link rel="photo" type="image/png" href="<?= htmlReady($user->getAvatar()->getURL(Avatar::NORMAL)) ?>"/>
+    <uri>acct:<?= htmlspecialchars($user['username']) ?>@<?= $_SERVER['SERVER_NAME'] ?></uri>
+    <name><?= htmlspecialchars($user->getName())?></name>
+    <link rel="photo" type="image/png" href="<?= htmlspecialchars($user->getAvatar()->getURL(Avatar::NORMAL)) ?>"/>
     <link rel="alternate" type="text/html" href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP']."dispatch.php/profile?username=".$user['username'] ?>"/>
   </author>
   <activity:actor>
@@ -13,14 +13,14 @@
     <id><?= $GLOBALS['ABSOLUTE_URI_STUDIP']."dispatch.php/profile?username=".$user['username'] ?></id>
     <title><?= $user->getName() ?></title>
     <link rel="alternate" type="text/html" href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP']."dispatch.php/profile?username=".$user['username'] ?>"/>
-    <link rel="avatar" type="image/png" href="<?= htmlReady($user->getAvatar()->getURL(Avatar::NORMAL)) ?>"/>
-    <link rel="photo" type="image/png" href="<?= htmlReady($user->getAvatar()->getURL(Avatar::NORMAL)) ?>"/>
+    <link rel="avatar" type="image/png" href="<?= htmlspecialchars($user->getAvatar()->getURL(Avatar::NORMAL)) ?>"/>
+    <link rel="photo" type="image/png" href="<?= htmlspecialchars($user->getAvatar()->getURL(Avatar::NORMAL)) ?>"/>
   </activity:actor>
   <activity:verb>http://activitystrea.ms/schema/1.0/follow</activity:verb>
   <activity:object>
     <activity:object-type>http://activitystrea.ms/schema/1.0/person</activity:object-type>
     <title><?= $whiterabbit->getName() ?></title>
-    <id><?= htmlReady($whiterabbit['data']['id']) ?></id>
-    <link rel="alternate" type="text/html" href="<?= htmlReady($whiterabbit->getOstatusID()) ?>"/>
+    <id><?= htmlspecialchars($whiterabbit['data']['id']) ?></id>
+    <link rel="alternate" type="text/html" href="<?= htmlspecialchars($whiterabbit->getOstatusID()) ?>"/>
   </activity:object>
 </entry>
--- views/webfinger/feed.php
+++ views/webfinger/feed.php
@@ -6,31 +6,31 @@
             xmlns:poco="http://portablecontacts.net/spec/1.0" 
             xmlns:ostatus="http://ostatus.org/schema/1.0" 
             xmlns:statusnet="http://status.net/schema/api/1/">
-  <generator uri="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>" version="<?= $GLOBALS['SOFTWARE_VERSION'] ?>"><?= studip_utf8encode(htmlReady($GLOBALS['UNI_NAME_CLEAN'])) ?></generator>
-  <id><?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/webfinger/feed/".$user['username']) ?></id>
-  <title><?= studip_utf8encode(htmlReady($user->getName())) ?> timeline</title>
-  <subtitle>Updates from <?= studip_utf8encode(htmlReady($user->getName())) ?> on <?= studip_utf8encode(htmlReady($GLOBALS['UNI_NAME_CLEAN'])) ?>!</subtitle>
+  <generator uri="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>" version="<?= $GLOBALS['SOFTWARE_VERSION'] ?>"><?= studip_utf8encode(htmlspecialchars($GLOBALS['UNI_NAME_CLEAN'])) ?></generator>
+  <id><?= htmlspecialchars($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/webfinger/feed/".$user['username']) ?></id>
+  <title><?= studip_utf8encode(htmlspecialchars($user->getName())) ?> timeline</title>
+  <subtitle>Updates from <?= studip_utf8encode(htmlspecialchars($user->getName())) ?> on <?= studip_utf8encode(htmlspecialchars($GLOBALS['UNI_NAME_CLEAN'])) ?>!</subtitle>
   <logo><?= $user->getAvatar()->getURL(AVATAR::NORMAL) ?></logo>
   <updated>2010-08-22T13:26:16+00:00</updated>
   <author>
-    <name><?= studip_utf8encode(htmlReady($user->getName())) ?></name>
+    <name><?= studip_utf8encode(htmlspecialchars($user->getName())) ?></name>
     <uri><?= $GLOBALS['ABSOLUTE_URI_STUDIP'].'dispatch.php/profile?username='.$user['username'] ?></uri>
   </author>
-  <link href="<?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/profile?user_id=".$user['user_id']) ?>" rel="alternate" type="text/html"/>
-  <link href="<?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/hub/register") ?>" rel="hub"/>
+  <link href="<?= htmlspecialchars($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/profile?user_id=".$user['user_id']) ?>" rel="alternate" type="text/html"/>
+  <link href="<?= htmlspecialchars($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/hub/register") ?>" rel="hub"/>
   <link href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>plugins.php/ostatus/salmon/endpoint" rel="salmon"/>
   <link href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>plugins.php/ostatus/salmon/endpoint" rel="http://salmon-protocol.org/ns/salmon-replies"/>
   <link href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>plugins.php/ostatus/salmon/endpoint" rel="http://salmon-protocol.org/ns/salmon-mention"/>
-  <link href="<?= htmlReady($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/webfinger/feed/".$user['username']) ?>" rel="self" type="application/atom+xml"/>
+  <link href="<?= htmlspecialchars($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/ostatus/webfinger/feed/".$user['username']) ?>" rel="self" type="application/atom+xml"/>
   <? foreach ($blubber as $blubb) : ?>
   <entry>
-    <title><?= studip_utf8encode(htmlReady($blubb['name'])) ?></title>
+    <title><?= studip_utf8encode(htmlspecialchars($blubb['name'])) ?></title>
     <link rel="alternate" type="text/html" href="<?= $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/thread/".$blubb['root_id'] ?>"/>
     <id><?= $blubb['root_id'] === $blubb['topic_id'] ? $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/thread/".$blubb['root_id'] : $GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/blubber/streams/comment/".$blubb->getId() ?></id>
     <published><?= date("c", $blubb['mkdate']) ?></published>
     <updated><?= date("c", $blubb['chdate']) ?></updated>
-    <content type="markdown"><?= studip_utf8encode(htmlReady($blubb['description'])) ?></content>
-    <content type="html"><?= studip_utf8encode(htmlReady(formatReady($blubb['description']))) ?></content>
+    <content type="markdown"><?= studip_utf8encode(htmlspecialchars($blubb['description'])) ?></content>
+    <content type="html"><?= studip_utf8encode(htmlspecialchars(formatReady($blubb['description']))) ?></content>
     <activity:verb>http://activitystrea.ms/schema/1.0/post</activity:verb>
     <activity:object-type><?= $blubb['root_id'] === $blubb['topic_id'] ? "http://activitystrea.ms/schema/1.0/note" : "http://activitystrea.ms/schema/1.0/comment" ?></activity:object-type>
     <statusnet:notice_info local_id="<?= $blubb->getId() ?>" source="web"></statusnet:notice_info>
