c8dbeca5249c529bf32d57718c77006366a93be2
Rasmus Fuhse <Krassmus@gmail.com>
2013-03-17 17:18:40 +0100

    debugging

--- views/feed/follow.php
+++ views/feed/follow.php
@@ -21,6 +21,6 @@
     <activity:object-type>http://activitystrea.ms/schema/1.0/person</activity:object-type>
     <title><?= $whiterabbit->getName() ?></title>
     <id><?= htmlspecialchars($whiterabbit['data']['id']) ?></id>
-    <link rel="alternate" type="text/html" href="<?= htmlspecialchars($whiterabbit->getOstatusID()) ?>"/>
+    <link rel="alternate" type="text/html" href="<?= htmlspecialchars($whiterabbit['data']['alias']) ?>"/>
   </activity:object>
 </entry>
